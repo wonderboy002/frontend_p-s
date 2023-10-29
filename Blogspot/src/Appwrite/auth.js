0b0becbd19419e85650e4e48ec1c0430fd9b3b9f
wonderboy002 <nipunkup@gmail.com>
2023-10-29 21:21:46 +0530

    Authentication made using appwrite

---
+++ Blogspot/src/Appwrite/auth.js
@@ -0,0 +1,55 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+export class authService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl).setProject(conf.projectId);
+    this.account = new this.Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const user = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (user) {
+        return this.account.login(email, password);
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAccount() {
+    try {
+      return this.account.get();
+    } catch (error) {
+      console.log("Appwrite error in getAccount()", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions("all");
+    } catch (error) {
+      console.log("error in logout() appwrite", error);
+    }
+  }
+}
+const Auth = new authService();
+export default Auth;
